0b7e999c95bd05d586b14ee4450101bad8d49d38
Daniël Klabbers <daniel@klabbers.email>
2022-07-31 00:19:28 +0200

    fix: without env the command fails

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,15 +2,10 @@
 
 namespace Flarum\Release;
 
-use Illuminate\Container\Container;
-use Illuminate\Contracts\View\Factory;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
-$container = Container::getInstance();
-
-$dotenv = new Dotenv;
-$dotenv->load(__DIR__ . '/../.env');
+file_exists(__DIR__ . '/../.env') && (new Dotenv)->load(__DIR__ . '/../.env');
 
 $app = new Application(
     'Flarum Release'
